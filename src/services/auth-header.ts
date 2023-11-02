e1deab8dd105037fd6f814e7063bc5e39dbc1659
Fredrik Lunde <fredrik.lunde3@gmail.com>
2023-11-02 15:27:34 +0100

    remove unneccesary comments

--- src/services/auth-header.ts
+++ src/services/auth-header.ts
@@ -4,8 +4,8 @@ export default function authHeader() {
   if (userStr) user = JSON.parse(userStr);
 
   if (user && user.accessToken) {
-    return { "x-access-token": user.accessToken }; // for Node.js Express back-end
+    return { "x-access-token": user.accessToken };
   } else {
-    return { "x-access-token": null }; // for Node Express back-end
+    return { "x-access-token": null };
   }
 }
